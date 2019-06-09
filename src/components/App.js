dc4f3e9f33515610057437a0d9fd2d3c833b1f2c
horlard <oladipuposaulih@gmail.com>
2019-06-09 19:34:15 +0100

    added react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Header from "./header";
 
 class App extends React.Component {
     render() {
         return(
-            <React.Fragment>
-                App
-            </React.Fragment>
+            <BrowserRouter>
+                <div className='container'>
+                     <Header />
+                     <div className='main_container'>
+                         <Route exact path='/' component={index} />
+                     </div>
+                 </div>
+            
+            
+            </BrowserRouter>
+            
         )
     }
 }
