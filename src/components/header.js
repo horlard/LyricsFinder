5f3a536819adcbe0384fafa6a275704dbbd0e5da
horlard <oladipuposaulih@gmail.com>
2019-06-09 19:15:31 +0100

    an header component to handle my routing

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header =()=> {
+    return (
+        <div className='header'>
+            <div className='header_text'>
+                LyricFinder
+            </div>
+        </div>
+    )
+}
