d1e148168fdc0ae9b08e323b02edfaab8191c537
horlard <oladipuposaulih@gmail.com>
2019-06-09 21:53:34 +0100

    did a huge refactor and made track.js call the api

--- src/components/tracks.js
+++ src/components/tracks.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Done} from '../actions';
+import Musix from '../api/musix';
+import {connect} from 'react-redux';
 
-
-const Tracks = () => {
-    return (
-        <div>
-            Tracks
-        </div>
-    )
+ class Tracks extends Component {
+    async componentDidMount() {
+        const response = await Musix.get('/chart.tracks.get',{
+            params : {
+                chart_name : 'top',
+                page : '1',
+                page_size : '10',
+                country : 'us',
+                f_has_lyrics : '1',
+                apikey : '03e1dfb7b02e443fce1505fc92d96178'  
+            }
+        })
+        console.log(response);
+        this.props.Done(response.data.message.body.track_list);
+        console.log(this.props.tracklist);
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
-export default Tracks;
+const mapStateToProps = (state) => {
+    return {tracklist : state.trackInfo}
+}
+
+export default connect(mapStateToProps,{Done})(Tracks);
+
+
+
+
