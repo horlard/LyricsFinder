14ed2dfe3c37c30c70d1125057d57681e12674b7
horlard <oladipuposaulih@gmail.com>
2019-06-10 21:55:04 +0100

    made a search form to the available lyrics

--- src/components/tracks.js
+++ src/components/tracks.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import {Done} from '../actions';
 import Musix from '../api/musix';
-import {connect} from 'react-redux';
+import '../style.css'
+import Track from './Track';
 
  class Tracks extends Component {
+     state = {
+         tracklists : []
+     }
     async componentDidMount() {
         const response = await Musix.get('/chart.tracks.get',{
             params : {
@@ -15,12 +18,14 @@ import {connect} from 'react-redux';
                 apikey : '03e1dfb7b02e443fce1505fc92d96178'  
             }
         })
-        this.props.Done(response.data.message.body.track_list);
-        console.log(this.props.tracklist);
+        this.setState({tracklists:response.data.message.body.track_list})
+        console.log(this.state.tracklists);
+        
+       
     }
     render() {
         return (
-            <div className='ui container'>
+            <div className='ui container' style={{marginTop:'30px'}}>
                 <div className='ui segment'>
                 <form className='ui form'>
                 <div className='field'>
@@ -30,15 +35,22 @@ import {connect} from 'react-redux';
                 </div>
                  </form>    
             </div>
+
+            <div>
+               {
+                   this.state.tracklists.map(tracklist=> {
+                       return <Track tracklist={tracklist}/>
+                   })
+               }
+            </div>
+
             </div>
         )
     }
 }
-const mapStateToProps = (state) => {
-    return {tracklist : state.trackInfo}
-}
 
-export default connect(mapStateToProps,{Done})(Tracks);
+
+export default Tracks;
 
 
 
