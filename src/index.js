714d1f6abdadd5d6650aa0bc9f68c5121239fdbf
horlard <oladipuposaulih@gmail.com>
2019-06-10 21:57:07 +0100

    removed all component for connecting to redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+
 
 ReactDOM.render(
-    <Provider store={createStore(Reducers)}>
-        <App />
-    </Provider>
-    ,
+    <App />,
     document.querySelector('#root')
 )
