2cb3b6027dd04a917cfc95684cca80e820aef2a1
horlard <oladipuposaulih@gmail.com>
2019-06-27 18:42:49 +0100

    did a huge refactor

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,35 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import Children from './children';
 
 
-const Track = (props)=> {
-  
-    return (
-
-        <div className='top_container'>
-            <div className='top_contain'>
-                <div className='artist_name' style={{marginBottom: '10px',fontSize:'20px'}}>
-                    {props.tracklist.track.artist_name}
-                </div>
-                <div className='track_name' style={{marginBottom: '5px'}}>
-                <i class="play icon"></i>Track:{props.tracklist.track.track_name}
-                </div>
-                
-                <div className='album_name'>
-                <i class="bullseye icon"></i> Album:{props.tracklist.track.album_name}
-                </div>
-                <Link to={`lyrics/tracks/${props.tracklist.track.track_id}`}>
-                <button class="ui primary button" style={{padding:'7px 85px',marginTop:'10px'}}>
-                <i class="angle right icon"></i>View lyrics
-                </button>
-                </Link>
-                
-            </div>
-        </div>
-            
-
-            
+class Track extends React.Component{
+    
+    render(){
         
-    )
+            return (
+
+                    <Children tracklist={this.props.tracklist}/>
+               
+            )
+            
+
+                
+                    
+        
+                    
+                
+            
+        }
+        
+    
+    
 }
 export default Track;
